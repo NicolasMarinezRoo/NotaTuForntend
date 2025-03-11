5f681f184a58e9d3c2aece7e088d2e432977bd63
NicolasMarinezRoo <nicolaso0567@gmail.com>
2025-03-11 10:21:23 +0100

    examen

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,5 @@
 import { fetchAPI } from "../utils/FetchAPI";
-const URL_BASE = 'http://localhost:3000/api/';
+const URL_BASE = import.meta.env.VITE_URL_BASE;
 
 export class UserService {
     static async getAll() {
