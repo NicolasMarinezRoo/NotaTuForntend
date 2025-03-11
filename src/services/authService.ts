b0dd843c8098a45c19771151904b10c768ff0ace
NicolasMarinezRoo <nicolaso0567@gmail.com>
2025-03-11 10:13:23 +0100

    examen

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,7 +2,8 @@
 import User from "../models/user2";
 import { fetchAPI } from "../utils/FetchAPI";
 
-const URL_BASE = 'http://localhost:3000/api/';
+//const URL_BASE = 'http://localhost:3000/api/';
+const URL_BASE = import.meta.env.VITE_URL_BASE
 export const loginUser = async (email: string, password: string) => {
 
     try {
